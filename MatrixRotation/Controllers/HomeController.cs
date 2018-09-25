2f6487f393a93ca6cadb46c3d37f5e55c80d9d99
araxis <oreshko2010@gmail.com>
2018-09-25 05:20:33 +0300

    1

---
+++ MatrixRotation/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MatrixRotation.Models;
+
+namespace MatrixRotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new MatrixViewModel(6));
+        }
+
+        public IActionResult Rotate(MatrixViewModel matrixModel)
+        {
+            matrixModel.Rotate();
+            return View("Index", matrixModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MatrixRotation/Models/MatrixViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatrixRotation.Models
+{
+    public class MatrixViewModel
+    {
+        public List<List<int>> Matrix;
+
+        public int Size { get; set; } = 5;
+
+        public MatrixViewModel()
+        {
+
+        }
+
+        public MatrixViewModel(int size)
+        {
+            Size = size;
+            var rnd = new Random();
+            Matrix = new List<List<int>>();
+            for (var i = 0; i < Size; i++)
+            {
+                var list = new List<int>();
+                for (var j = 0; j < Size; j++)
+                {
+                    list.Add(rnd.Next(1, 100));
+                }
+                Matrix.Add(list);
+            }
+        }
+
+        public void Rotate()
+        {
+
+        }
+    }
+}
